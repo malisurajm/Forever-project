ed580ca14d571f02c4bb18d652c41150ec21507f
Suraj Mali <surajmali22127@gmail.com>
2025-09-03 17:05:06 +0530

    Fixed admin login: check email & password correctly

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -84,7 +84,7 @@ const adminLogin = async (req,res) => {
          
        const {email,password} = req.body
 
-       if(email === process.env.ADMIN_EMAIl && password === process.env.ADMIN_PASSWORD) {
+       if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
           const token = jwt.sign(email+password,process.env.JWT_SECRET);
           res.json({success:true,token})
        } else {
